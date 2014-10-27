9c65668524c7179d34c08941c037c882c9a0f582
Jakub Misterka <san.y4ku@gmail.com>
2014-10-26 23:52:49 -0500

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -7,13 +7,13 @@
 - (void)initialize
 {
     // your code here
+    character = (Character*)[CCBReader load:@"Character"];
+    [physicsNode addChild:character];
 }
 
 -(void)update:(CCTime)delta
 {
     // put update code here
-    character = (Character*)[CCBReader load:@"Character"];
-    [physicsNode addChild:character];
 }
 
 // put new methods here
